f7bf96a51e2e4bc14df48854eaa428b172910664
Tod9197 <dancein18@yahoo.co.jp>
2025-08-20 19:11:59 +0900

    問37,38を実装

---
+++ src/review1-8.ts
@@ -0,0 +1,14 @@
+export {};
+// ### 37. ライブラリインストール
+// `@types/node` をインストールしてください。(npm / yarn)
+// package.jsonが作成されることを確認してください。
+
+// ### 38. package.jsonについて
+// 以下のスクリプトを `scripts` に追加してください。
+// |スクリプト名|コマンド|
+// |---|---|
+// |`start`|`node index.js`|
+// |`build`|`tsc`|
+
+// - `npm run start` を実行してください
+// - `npm run build` を実行してしてください
